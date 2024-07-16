a500562d292e25ab0147da4ff2d36700d1fe7bfb
Aryan <ariyasaran200@gmail.com>
2024-07-16 19:20:50 +0530

    summary file open

--- Raptor/raptorIMP.py
+++ Raptor/raptorIMP.py
@@ -1,9 +1,13 @@
-import tiktoken
+from typing import Optional
+import numpy as np
+import umap
+import pandas as pd
 from langchain_community.document_loaders import DirectoryLoader
-from sentence_transformers import SentenceTransformer
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sklearn.mixture import GaussianMixture
 from embeddings import model_emb_st
 from helper import get_token_count
+
 #loading the txt files 
 loader = DirectoryLoader('PATH TO THE DIR', glob="**/*.txt")
 docs = loader.load()
@@ -28,6 +32,60 @@ concatenated_content = "\n\n\n --- \n\n\n".join(
 
 global_embeddings = [model_emb_st.encode(txt) for txt in texts]
 
+def reduce_cluster_embeddings(
+    embeddings: np.ndarray,
+    dim: int,
+    n_neighbors: Optional[int] = None,
+    metric: str = "cosine",
+) -> np.ndarray:
+    if n_neighbors is None:
+        n_neighbors = int((len(embeddings) - 1) ** 0.5)
+    return umap.UMAP(
+        n_neighbors=n_neighbors, n_components=dim, metric=metric
+    ).fit_transform(embeddings)
+
+dim = 2
+global_embeddings_reduced = reduce_cluster_embeddings(global_embeddings, dim)
+
+def get_optimal_clusters(embeddings: np.ndarray, max_clusters: int = 50, random_state: int = 1234):
+    max_clusters = min(max_clusters, len(embeddings))
+    bics = [GaussianMixture(n_components=n, random_state=random_state).fit(embeddings).bic(embeddings)
+            for n in range(1, max_clusters)]
+    return np.argmin(bics) + 1
+
+def gmm_clustering(embeddings: np.ndarray, threshold: float, random_state: int = 0):
+    n_clusters = get_optimal_clusters(embeddings)
+    gm = GaussianMixture(n_components=n_clusters, random_state=random_state).fit(embeddings)
+    probs = gm.predict_proba(embeddings)
+    labels = [np.where(prob > threshold)[0] for prob in probs]
+    return labels, n_clusters
+
+
+labels, _ = gmm_clustering(global_embeddings_reduced, threshold=0.5)
+
+simple_labels = [label[0] if len(label) > 0 else -1 for label in labels]
+
+df = pd.DataFrame({
+    'Text': texts,
+    'Embedding': list(global_embeddings_reduced),
+    'Cluster': simple_labels
+})
+print(df.head(3))
+def format_cluster_texts(df):
+    clustered_texts = {}
+    for cluster in df['Cluster'].unique():
+        cluster_texts = df[df['Cluster'] == cluster]['Text'].tolist()
+        clustered_texts[cluster] = " --- ".join(cluster_texts)
+    return clustered_texts
+
+clustered_texts = format_cluster_texts(df)
 
 
 
+####
+from summarizer import process_with_llama
+
+summaries = {}
+for cluster, text in clustered_texts.items():
+    summary = process_with_llama(text)
+    summaries[cluster] = summary
---
+++ Raptor/summarizer.py
@@ -0,0 +1,45 @@
+"""
+This file has diffrent ways to summarize the text
+[open-source , openai api, langchain+openai]
+"""
+
+
+# OPENSOURCE/FREE
+from transformers import AutoTokenizer, AutoModelForCausalLM,AutoModelForSeq2SeqLM,BitsAndBytesConfig
+
+def t5_summary(text):
+    tokenizer = AutoTokenizer.from_pretrained("google-t5/t5-base")
+    model = AutoModelForSeq2SeqLM.from_pretrained("google-t5/t5-base")
+    inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=512, truncation=True)
+    summary_ids = model.generate(inputs, max_new_tokens=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return summary
+
+
+# GPU Needed take 22 gb [use kaggle]
+#TODO add quantize modela and tinyops support (https://github.com/Guney-olu/tinyOPS)
+def llama_summary(text):
+    model_id =  "SalmanFaroz/Llama-2-7b-samsum"
+    model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config,device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+    prompt = f"""You are an assistant to create a detailed summary of the text input prodived.
+            Text:
+            {text}
+            """
+    inputs = tokenizer(prompt, return_tensors='pt', max_length=512, truncation=True, padding=True)
+    input_ids = inputs["input_ids"].to(model.device)
+    attention_mask = inputs["attention_mask"].to(model.device)
+
+    summary_ids = model.generate(
+        input_ids,
+        attention_mask=attention_mask,
+        max_new_tokens=300,
+        min_length=40,
+        length_penalty=2.0,
+        num_beams=4,
+        early_stopping=True
+    )
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return summary
